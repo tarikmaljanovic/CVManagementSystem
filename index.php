2bc2a80ccfb7d87c3c50c4d76835487de7d43d05
Tarik Maljanovic <tarikmaljanovic123@gmail.com>
2023-03-15 20:37:43 +0100

    Login page designed

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
   require "vendor/autoload.php";
-
-  Flight::route('/', function() {
-    echo "hello";
-  });
-
   
 
   Flight::start();
---
+++ js_files/login.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".signup-switch").click(function() {
+    $(".login-box").remove("hidden");
+    $(".signup-box").removeClass("hidden");
+  });
+
+  $(".login-switch").click(function() {
+  });
+})
