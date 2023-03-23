49b802ddcdd8ea693216b8635f517a24188a038f
Tarik Maljanovic <tarikmaljanovic123@gmail.com>
2023-03-23 02:19:55 +0100

    mobile sidebar added

--- js_files/login.js
+++ js_files/login.js
@@ -2,35 +2,14 @@ $(document).ready(function() {
   $(".signup-switch").click(function() {
     $(".login-box").addClass("hidden");
     $(".signup-box").removeClass("hidden");
+
     return false;
   });
 
   $(".login-switch").click(function() {
     $(".login-box").removeClass("hidden");
     $(".signup-box").addClass("hidden");
+    
     return false;
   });
-
-  $(".signup-button").click(function() {
-    if($(".first-name-input").text() == "" || $(".last-name-input").text() == "" || $(".email-signup-input").text() == ""
-    || $(".password-signup-input").text() == "" || $(".password-conf-input").text() == "") {
-      $(".first-name, .last-name, .email-signup, .password-signup, .password-conf").css({"color" : "red", "font-weight" : "600"});
-      $(".first-name-input, .last-name-input, .email-signup-input, .password-signup-input, .password-conf-input").css({"border" : "solid red 1px"});
-    }
-
-    //Signup Code
-
-    return false;
-  });
-
-  $(".login-button").click(function() {
-    if($(".email-login-input").text() == "" || $(".password-login-iput").text() == "") {
-      $(".email-login, .password-login").css({"color" : "red", "font-weight" : "600"});
-      $(".email-login-input, .password-login-input").css({"border" : "solid red 1px"});
-    }
-
-    //Login Code
-
-    return false;
-  })
 })
--- js_files/profile.js
+++ js_files/profile.js
@@ -3,5 +3,18 @@ $(document).ready(function() {
        $(".dropdown ").toggleClass("is-active");
 
         return false;
+    });
+
+    $(".navbar-burger").click(function() {
+        $(".sidebar").removeClass("hidden");
+
+        return false;
+    });
+
+    $(document).click(function() {
+        $(".sidebar").addClass("hidden");
+        $(".dropdown").removeClass("is-active");
+
+        return false;
     })
 })
