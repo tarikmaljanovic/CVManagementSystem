acc7bacd4b8a24fe76dfb154bc6392e659887a75
Tarik Maljanovic <tarikmaljanovic123@gmail.com>
2023-03-30 05:21:14 +0200

    aesthetic fixing

---
+++ rest/dao/SkillDao.class.php
@@ -0,0 +1,20 @@
+<?php
+  class SkillDao {
+    private $conn;
+
+    public function __construct() {
+      try {
+        $servername = "127.0.0.1";
+        $username= "root";
+        $password = "stardust";
+        $schema = "cv_management";
+
+        $this->conn = new PDO ("mysql:host=$servername;dbname=$schema",$username, $password);
+        
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch(PDOException $e) {
+        echo "Connection failed " . $e->getMessage();
+      }
+    }
+  }
+?>
