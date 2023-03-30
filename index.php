18c66b434d37cc926022b41dd75746445219005a
Tarik Maljanovic <tarikmaljanovic123@gmail.com>
2023-03-30 04:23:56 +0200

    fiexd login buttons bug

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
   require 'vendor/autoload.php';
+  require 'rest/dao/UserDao.class.php';
+  header('Access-Control-Allow-Origin: *');
+  header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS, PATCH');
+
+  Flight::register('userDao', 'UserDao');
 
   Flight::route('/', function() {
     require 'login.html';
--- js_files/login.js
+++ js_files/login.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
   $("#signup-switch").click(function() {
-    $(".login-box").hide();
-    $(".signup-box").show();
+    $(".login-box").addClass("hidden");
+    $(".signup-box").removeClass("hidden");
 
     return false;
   });
 
   $("#login-switch").click(function() {
-    $(".login-box").show();
-    $(".signup-box").hide();
+    $(".login-box").removeClass("hidden");
+    $(".signup-box").addClass("hidden");
 
     return false;
-  })
-})
+  });
+
+  $(".login-box").click(function(event) {
+    return false;
+  });
+  $(".signup-box").click(function(event) {
+    return false;
+  });
+});
