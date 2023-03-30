b108feac6f3f4e9bf2734a18674fdaa87151bb29
Tarik Maljanovic <tarikmaljanovic123@gmail.com>
2023-03-30 02:09:13 +0200

    .

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
   require 'vendor/autoload.php';
+<<<<<<< HEAD
   require 'dao/UserDao.class.php';
   header('Access-Control-Allow-Origin: *');
   header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS, PATCH');
@@ -12,6 +13,11 @@
 
   Flight::route('/profile', function() {
     require 'profile.html';
+=======
+
+  Flight::route('/', function() {
+    require 'login.html';
+>>>>>>> c8c59f4 (.)
   });
 
   Flight::start();
