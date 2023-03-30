07ccd4bf058f21a398c35b7002784f43f810c2a9
Tarik Maljanovic <tarikmaljanovic123@gmail.com>
2023-03-30 11:21:19 +0200

    Moved index.php into rest

--- rest/index.php
+++ rest/index.php
@@ -1,13 +1,17 @@
 <?php
-  require 'vendor/autoload.php';
-  require 'rest/dao/UserDao.class.php';
+  require '../vendor/autoload.php';
+  require 'dao/UserDao.class.php';
   header('Access-Control-Allow-Origin: *');
   header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS, PATCH');
 
   Flight::register('userDao', 'UserDao');
 
   Flight::route('/', function() {
-    require 'login.html';
+    require '../login.html';
+  });
+
+  Flight::route('GET /profile', function() {
+    require '../profile.html';
   });
 
   Flight::start();
