967f3a7f9d7e107cda320017ab2d7afca1be83c3
naimPJ <private.naim@gmail.com>
2023-05-21 03:50:03 +0200

    removing unused routes

--- rest/routes/UserRoutes.php
+++ rest/routes/UserRoutes.php
@@ -3,37 +3,6 @@
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
-Flight::route("GET /users", function(){
-    Flight::json(Flight::userService()->get_all());
- });
- 
- Flight::route("GET /user_by_id", function(){
-    Flight::json(Flight::userService()->get_by_id(Flight::request()->query['id']));
- });
- 
- Flight::route("GET /users/@id", function($id){
-    Flight::json(Flight::userService()->get_by_id($id));
- });
- 
- Flight::route("DELETE /users/@id", function($id){
-    Flight::userService()->delete($id);
-    Flight::json(['message' => "User deleted successfully"]);
- });
- 
- Flight::route("POST /user", function(){
-    $request = Flight::request()->data->getData();
-    Flight::json(['message' => "User added successfully",
-                  'data' => Flight::userService()->add($request)
-                 ]);
- });
- 
- Flight::route("PUT /user/@id", function($id){
-    $student = Flight::request()->data->getData();
-    Flight::json(['message' => "User edit successfully",
-                  'data' => Flight::userService()->update($student, $id)
-                 ]);
- });
-
  Flight::route('POST /register', function(){
    $data = Flight::request()->data->getData();
    $data['passwrd'] = md5($data['passwrd']);
