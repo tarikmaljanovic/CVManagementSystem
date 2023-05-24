482967ac4c4cef473bf9905c9573a6d19db4ca6a
naimPJ <private.naim@gmail.com>
2023-05-24 03:28:48 +0200

    dao fixing

--- rest/dao/CVsDao.class.php
+++ rest/dao/CVsDao.class.php
@@ -6,8 +6,8 @@
       parent::__construct("cvs");
     }
 
-    public function getCvsByUser($id) {
-      $result = $this->query("SELECT * FROM cvs WHERE user_id = '$id'");
+    public function getCvsByUser($userId) {
+      return $this->query("SELECT * FROM cvs WHERE user_id = :userId ", ['userId' => $userId]);
       return $result;
     }
   }
